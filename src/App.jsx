a6755b19b03845d4dee16989f0fbad83887cfdcf
Anton Tigunov <seyanaracore@gmail.com>
2022-07-28 15:36:08 +0300

    CRA Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
-// import Header from './Components/Header';
+import '@/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">New project</header>
-      {/* <Header /> */}
     </div>
   );
 }
